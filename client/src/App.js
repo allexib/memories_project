6433c58212b4d8451c5099792cc82851acea8375
allexib <ooomegasib@gmail.com>
2022-04-23 16:10:57 +0300

    memories png, styles.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import {Container, AppBar, Typography, Grow, Grid} from '@mui/material'
 
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
-import memories from './images/memories.jpg'
+import memories from './images/memories.png'
+import useStyles from './styles'
 
 const App = () => {
+    const classes=useStyles()
+
     return (
-        <Container maxidth='lg'>
-            <AppBar position='static' color='inherit'>
-                <Typography variant='h2' align='center'> Memories </Typography>
-                <img src={memories} alt='memories' height='400'/>
+        <Container maxWidth='lg'>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+                <img className={classes.image} src={memories} alt='memories' height='60'/>
             </AppBar>
             <Grow in>
                 <Container>
